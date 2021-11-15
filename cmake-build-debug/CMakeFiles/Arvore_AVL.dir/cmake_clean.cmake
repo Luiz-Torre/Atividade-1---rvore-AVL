f7ca4d9daa3827ca5dd26b68d7c2f9bb30d490ea
HunterXVD11 <81260156+HunterXVD11@users.noreply.github.com>
2021-11-15 15:29:12 -0300

    Criando ambiente para multiplos projetos

---
+++ cmake-build-debug/CMakeFiles/Arvore_AVL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Arvore_AVL.exe"
+  "Arvore_AVL.exe.manifest"
+  "Arvore_AVL.pdb"
+  "CMakeFiles/Arvore_AVL.dir/AtividadeAVL.c.obj"
+  "libArvore_AVL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Arvore_AVL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
